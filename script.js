24a259cae1383430b2f4e567c681e80e4f761615
unknown <marxtai94@gmail.com>
2014-11-03 16:19:51 -0500

    Fixed the problem with expanding in first word of new line

--- script.js
+++ script.js
@@ -20,11 +20,20 @@ $(function(){
 	replaceSnippets["caps_p"] = "Hi,\n\nPlease do not post all in caps as it's classed as shouting and is not appreciated.\n\nThank you for your comprehension.\n\nRegars,\n" + user;
 	replaceSnippets["caps_n"] = "FR given not to post all in caps.";
 
+	replaceSnippets["rel"]= "Please do not ask for release dates in WIP topics, the mod will be released when it's finished.\n\nRefer to the WIP rules and guidelines : http://fs-uk.com/forum/index.php?topic=98798.0";
+	replaceSnippets["rel_p"] = "Hi,\n\nPlease do not ask for release dates in WIP topics. The modder will release the mod when it's finished.\n\nThank you for your comprehension.\n\nRegards,\n" + user;
+	replaceSnippets["rel_n"] = "FR given not to ask for updates/release date in WIP topic.";
+
 	insertSnippets["rules"] = forumRules;
 	insertSnippets["wrules"]= "http://fs-uk.com/forum/index.php?topic=98798.0";
 	insertSnippets["url"] = "[url=XXX]displayedText[/url]";
 	insertSnippets["img"] = "[img]linkToImage[/img]";
 	insertSnippets["code"] = "[code[i][/i]][[i][/i]/code]";
+
+	insertSnippets["fmod13"] = "http://fs-uk.com/forum/index.php?topic=124265.0";
+	insertSnippets["fmap13"] = "http://fs-uk.com/forum/index.php?topic=126665.0";
+	insertSnippets["fmod"] = "http://fs-uk.com/forum/index.php?topic=165712.0";
+	insertSnippets["fmap"] = "http://fs-uk.com/forum/index.php?topic=166163.0";
 
 
 	//Event for keypress on the input zone
@@ -36,12 +45,17 @@ $(function(){
 		{
 			//We get the complete last word
 			var lastWord = content.split(" ").pop();
+			lastWord = lastWord.split("\n").pop();
 
 			//We remove the modifier, to check in the arrays (one instead of one<)
-			var lastWordCleaned = lastWord.substr(0, lastWord.length - 1);
+			var lastWordCleaned = lastWord.substr(0, lastWord.length - 1);			
 
 			//We remove the white spaces and newlines
 			var lastWordTrimmed = jQuery.trim(lastWordCleaned);
+
+			console.log("lastWord (brut) :" + lastWord);
+			console.log("lastWordCleaned : " + lastWordCleaned);
+			console.log("lastWordTrimmed : " + lastWordTrimmed);
 
 			//Is it in the replace array?
 			if(replaceSnippets[lastWordTrimmed] != undefined)
