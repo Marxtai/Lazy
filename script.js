409d05b796816b3dfe7716c24e65fe7ce935a15e
Charles <marxtai94@gmail.com>
2014-11-20 13:55:56 -0500

    No more modifier key or replaceSnippets
    You can now expand tags by pressing the tab key instead of using a modifier key (like the "real" Emmet/ZenCoding).
    There are no more replaceSnippets, only insertSnippets that will replace the last word by the value.

--- script.js
+++ script.js
@@ -1,98 +1,52 @@
 $(function(){
-	var modifier = "<"; //Modifier key used to mark the word as a keyword
-	var user = "yourUsername"; //Username used to sign your PMs
-	var textarea = $('textarea'); 
 
-	var replaceSnippets = [];
-	var insertSnippets = [];
+			var insertSnippets = [];
 
-	var forumRules = "http://fs-uk.com/forum/index.php?topic=120776.0";
+			insertSnippets["rules"] = "http://fs-uk.com/forum/index.php?topic=120776.0";
+			insertSnippets["url"] = "[url=XXX]displayedText[/url]";
+			insertSnippets["img"] = "[img]linkToImage[/img]";
+			insertSnippets["code"] = "[code[i][/i]][[i][/i]/code]";
 
-	replaceSnippets["one"] = "Please do not make one word posts, we like to see more thoughts in your posts.\n\nRefer to the forum rules and guidelines : " + forumRules;
-	replaceSnippets["one_p"] = "Hi,\n\nPlease do not make one word posts as it's against the forum rules.\n\nThank you for your comprehension.\n\nRegards,\n" + user;
-	replaceSnippets["one_n"] = "FR given not to make one word posts.";
+			$('#post').on('keydown', function(e){
+				if(e.keyCode == 9)
+				{
+					e.preventDefault();
 
-	replaceSnippets["req"] = "Please do not make mod requests as it's against the forum rules. " + forumRules;
-	replaceSnippets["req_p"] = "Hi,\n\nPlease do not make mod requests as it's against the forum rules and not appreciated.\n\nThank you for your comprehension.\n\nRegards,\n" + user;
-	replaceSnippets["req_n"] = "FR given not to make mod requests.";
+					//We get the last word of the content
+					var lastWord = $(this).val().split(" ").pop();
+					lastWord = lastWord.split("\n").pop();				
+					
+					//We check to see if it's in the array
+					if(insertSnippets[lastWord] != undefined)
+					{
+						var content = $(this).val();
+						content = content.replace(jQuery.trim(lastWord), insertSnippets[lastWord]);
+						$(this).val(content);
+					}
+				}
+			});
 
-	replaceSnippets["caps"] = "Please do not post all in caps as it's considered shouting and not appreciated.";
-	replaceSnippets["caps_p"] = "Hi,\n\nPlease do not post all in caps as it's classed as shouting and is not appreciated.\n\nThank you for your comprehension.\n\nRegars,\n" + user;
-	replaceSnippets["caps_n"] = "FR given not to post all in caps.";
+			//FS-UK Test Report visual helper
 
-	replaceSnippets["rel"]= "Please do not ask for release dates in WIP topics, the mod will be released when it's finished.\n\nRefer to the WIP rules and guidelines : http://fs-uk.com/forum/index.php?topic=98798.0";
-	replaceSnippets["rel_p"] = "Hi,\n\nPlease do not ask for release dates in WIP topics. The modder will release the mod when it's finished.\n\nThank you for your comprehension.\n\nRegards,\n" + user;
-	replaceSnippets["rel_n"] = "FR given not to ask for updates/release date in WIP topic.";
+			var select = $('.reportList select');	
 
-	insertSnippets["rules"] = forumRules;
-	insertSnippets["wrules"]= "http://fs-uk.com/forum/index.php?topic=98798.0";
-	insertSnippets["url"] = "[url=XXX]displayedText[/url]";
-	insertSnippets["img"] = "[img]linkToImage[/img]";
-	insertSnippets["code"] = "[code[i][/i]][[i][/i]/code]";
-
-	insertSnippets["fmod13"] = "http://fs-uk.com/forum/index.php?topic=124265.0";
-	insertSnippets["fmap13"] = "http://fs-uk.com/forum/index.php?topic=126665.0";
-	insertSnippets["fmod"] = "http://fs-uk.com/forum/index.php?topic=165712.0";
-	insertSnippets["fmap"] = "http://fs-uk.com/forum/index.php?topic=166163.0";
-
-
-	//Event for keypress on the input zone
-	textarea.on('keydown', function(){
-		var content = $(this).val();
-
-		//Is the last char the modifier?
-		if(modifier == content.substr(content.length - 1, content.length))
-		{
-			//We get the complete last word
-			var lastWord = content.split(" ").pop();
-			lastWord = lastWord.split("\n").pop();
-			lastWord = lastWord.split("\r").pop();
-			lastWord = lastWord.split("\r\n").pop();
-
-			//We remove the modifier, to check in the arrays (one instead of one<)
-			var lastWordCleaned = lastWord.substr(0, lastWord.length - 1);			
-
-			//We remove the white spaces and newlines
-			var lastWordTrimmed = jQuery.trim(lastWordCleaned);			
-
-			//Is it in the replace array?
-			if(replaceSnippets[lastWordTrimmed] != undefined)
-			{
-				//We replace the content of the entire textarea by the value of the snippet
-				$(this).val(replaceSnippets[lastWordTrimmed]);
-			}
-
-			//... or maybe it's in the insert array?
-			if(insertSnippets[lastWordTrimmed] != undefined)
-			{	
-				content = content.replace(jQuery.trim(lastWord), insertSnippets[lastWordTrimmed]);
-				$(this).val(content);
-			}
-		}
-	});	
-
-	// ======== TEST REPORTS ==========
-	// Visual feedback on the test reports item description
-
-	var select = $('.reportList select');	
-
-	select.on('change', function(){
-		switch($(this).val())
-		{
-			//Many problems
-			case "0":
-				$(this).css('color', 'red');
-				break;
-			//No problems
-			case "1":
-				$(this).css('color', 'green');
-				break;
-			//Some problems
-			case "3":
-				$(this).css('color', 'orange');
-				break;
-			default:
-				$(this).css('color', '#000');
-		}	
-	});
-});
+			select.on('change', function(){
+				switch($(this).val())
+				{
+					//Many problems
+					case "0":
+						$(this).css('color', 'red');
+						break;
+					//No problems
+					case "1":
+						$(this).css('color', 'green');
+						break;
+					//Some problems
+					case "3":
+						$(this).css('color', 'orange');
+						break;
+					default:
+						$(this).css('color', '#000');
+				}	
+			});
+		});
