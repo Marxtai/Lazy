ec5b83e6b6c4365f482cb2e6b7fb546442ba60e8
Charles <charles@Charles-PC.(none)>
2014-11-20 21:11:29 -0500

    Fixed the keywords conflicting

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
 $(function(){
+    var snippets = [];
 
-			var insertSnippets = [];
+    //Links to topics
+    snippets["rules"] = "http://fs-uk.com/forum/index.php?topic=120776.0";
+    snippets["wrules"] = "http://fs-uk.com/forum/index.php?topic=98798.0";
+    snippets["fmod"] = "http://fs-uk.com/forum/index.php?topic=165712.0";
+    snippets["fmap"] = "http://fs-uk.com/forum/index.php?topic=166163.0";
+    snippets["fmod13"] = "http://fs-uk.com/forum/index.php?topic=124265.0";
+    snippets["fmap13"] = "http://fs-uk.com/forum/index.php?topic=126665.0";
 
-			insertSnippets["rules"] = "http://fs-uk.com/forum/index.php?topic=120776.0";
-			insertSnippets["url"] = "[url=XXX]displayedText[/url]";
-			insertSnippets["img"] = "[img]linkToImage[/img]";
-			insertSnippets["code"] = "[code[i][/i]][[i][/i]/code]";
+    //BBCode shortcuts
+    snippets["img"] = "[img]linkToImage[/img]";
+    snippets["url"] = "[url=link]displayedText[/url]";
+    snippets["code"] = "[code[i][/i]][[i][/i]/code]";
+    snippets["b"] = "[b]someText[/b]";
+    snippets["i"] = "[i]someText[/i]";
+    snippets["u"] = "[u]someText[/u]";
 
-			insertSnippets["fmod13"] = "http://fs-uk.com/forum/index.php?topic=124265.0";
-			insertSnippets["fmap13"] = "http://fs-uk.com/forum/index.php?topic=126665.0";
-			insertSnippets["fmod"] = "http://fs-uk.com/forum/index.php?topic=165712.0";
-			insertSnippets["fmap"] = "http://fs-uk.com/forum/index.php?topic=166163.0";
+    //Other stuff
+    snippets["one"] = "Please do not make one word posts as it's against the forum rules. We like to see more thoughts in your posts.\n\nRefer to the forum rules and guidelines : http://fs-uk.com/forum/index.php?topic=120776.0";
 
-			$('textarea').on('keydown', function(e){
-				if(e.keyCode == 9)
-				{
-					e.preventDefault();
+    $('textarea').on('keydown', function(event){
+        if(event.keyCode == 9)
+        {
+            event.preventDefault();
 
-					//We get the last word of the content
-					var lastWord = $(this).val().split(" ").pop();
-					lastWord = lastWord.split("\n").pop();				
-					
-					//We check to see if it's in the array
-					if(insertSnippets[lastWord] != undefined)
-					{
-						var content = $(this).val();
-						content = content.replace(lastWord, insertSnippets[lastWord]);
-						$(this).val(content);
-					}
-				}
-			});
+            var content = $(this).val();
+            var lastWord = content.split(/\s+/).pop();                  
 
-			//FS-UK testing report visual helper			
+            //Is it a snippet?
+            if(snippets[lastWord] != undefined)
+            {
+                newString = content.substring(0, content.lastIndexOf(lastWord)) + snippets[lastWord];
+                $(this).val(newString);
+            }           
+        }
+    });
 
-			var select = $('.reportList select');	
+    //FS-UK testing report visual helper            
 
-			select.on('change', function(){
-				switch($(this).val())
-				{
-					//Many problems
-					case "0":
-						$(this).css('color', 'red');
-						break;
-					//No problems
-					case "1":
-						$(this).css('color', 'green');
-						break;
-					//Some problems
-					case "3":
-						$(this).css('color', 'orange');
-						break;
-					default:
-						$(this).css('color', '#000');
-				}	
-			});
-		});
+    var select = $('.reportList select');   
+
+    select.on('change', function(){
+        switch($(this).val())
+        {
+            //Many problems
+            case "0":
+                $(this).css('color', 'red');
+                break;
+            //No problems
+            case "1":
+                $(this).css('color', 'green');
+                break;
+            //Some problems
+            case "3":
+                $(this).css('color', 'orange');
+                break;
+            default:
+                $(this).css('color', '#000');
+        }   
+    });
+});
